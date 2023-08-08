f0224a40d4eb11b8a19271160cde20b21ffe1381
sunw <qitiandasheng5259@126.com>
2023-08-08 22:35:04 +0800

    feat: 调试虚拟树

--- src/components/virtual-project-tree/virtual-tree.vue
+++ src/components/virtual-project-tree/virtual-tree.vue
@@ -2,7 +2,7 @@
   <div class="virtual-tree-wrapper h-100-104 box-content overflow-auto px-16 relative bg-color-white">
     <div
       class="v-tree absolute top-0 left-0 right-0 bottom-0 px-16"
-      :style="{transform: `translateY(${scrollTop}px)`}"
+      :style="{transform: `translateY(${vtreeMoveDistance}px)`}"
     >
       <div
         v-for="item in renderedTreeData"
@@ -36,55 +36,26 @@ import { treeFlatten, } from './utils'
 import { businessTreeMock } from './mock';
 
 const scrollTop = ref(0)
-const renderedTreeData = ref([])
+const renderedTreeData = ref<any[]>([])
 const treeMap = treeFlatten(businessTreeMock)
 const treeFullHeight = ref('0')
-const treeFullDomList = []
+const vtreeMoveDistance = ref(0)
 
 const start = ref(0)
-
-const createVTreeItemDom = (item) => {
-  const itemDiv = document.createElement('div')
-  itemDiv.className = 'border-t-0 border-l-0 border-r-0 border-b-1 border-solid border-b-color-grep-2 box-border min-h-44 leading-44 relative cursor-pointer flex items-center justify-between'
-
-  const textDiv = document.createElement('div')
-  textDiv.className = 'min-h-24 leading-24 inline-block pr-34'
-  if (item.domHeight > 44) {
-    textDiv.classList.add('px-7')
-  }
-  textDiv.style.setProperty('padding-left', `${item.level * 16}px`)
-  const textSpan = document.createElement('span')
-  textSpan.className = 'text-16 font-normal font-family'
-  item.level < 3 ? textSpan.classList.add('font-color-3') : textSpan.classList.add('font-color-2')
-  textSpan.textContent = item.name
-  textDiv.appendChild(textSpan)
-  itemDiv.appendChild(textDiv)
-
-  const iconDiv = document.createElement('div')
-  iconDiv.className = 'w-18 relative'
-  const iconInner = document.createElement('div')
-  iconInner.className = 'project-line-arrow'
-  iconDiv.appendChild(iconInner)
-  itemDiv.appendChild(iconDiv)
-
-  return itemDiv
-}
 
 const calculateVirtualTreeData = () => {
   const vtreeWrapDom = document.querySelector('.virtual-tree-wrapper')
   if (!vtreeWrapDom) return
 
-  const vtwCSSStyle = window.getComputedStyle(vtreeWrapDom)
-  const fontSize = +vtwCSSStyle.getPropertyValue('font-size').replace(/(px)$/i, '')
   const { height: vtwHeight, } = vtreeWrapDom.getBoundingClientRect()
-  const paddingLeft = +vtwCSSStyle.getPropertyValue('padding-left').replace(/(px)$/i, '')
-  const paddingRight = +vtwCSSStyle.getPropertyValue('padding-right').replace(/(px)$/i, '')
 
   const vtwRealHeightDom = document.querySelector('.virtual-tree-wrapper .real-height-div')
+  if (!vtwRealHeightDom) return
+
   let { width: vtwWidth } = vtwRealHeightDom.getBoundingClientRect()
 
   vtwWidth -= 6 // 估算移动端纵向滚动条宽度6px
-  const calcDomHeight = (name, level) => {
+  const calcDomHeight = (name: string, level: number) => {
     const nameLen = name.length * 16
     const boxLen = vtwWidth - level * 16 - 34
     if (nameLen <= boxLen) {
@@ -97,7 +68,29 @@ const calculateVirtualTreeData = () => {
     }
   }
 
-  const calcCount = (start, threshold) => {
+  const calcUpHiddenCount = (start: number, threshold: number) => {
+    let count = 0
+    let calcItemHeight = 0
+    let lastItemHeight = 0
+    for (let i = start, len = treeMap.length; i < len; i++) {
+      if (calcItemHeight >= threshold) {
+        if (i === 0) break
+        count--
+        lastItemHeight = treeMap[i - 1][1].domHeight
+        break
+      }
+      const item = treeMap[i][1]
+      if (item.domHeight === 0) {
+        item.domHeight = calcDomHeight(item.name, item.level)
+      }
+      calcItemHeight += item.domHeight
+      count++
+    }
+
+    return [ count, calcItemHeight, lastItemHeight ]
+  }
+
+  const calcCount = (start: number, threshold: number) => {
     let count = 0
     let calcItemHeight = 0
     for (let i = start, len = treeMap.length; i < len; i++) {
@@ -128,51 +121,48 @@ const calculateVirtualTreeData = () => {
   }
 
   treeFullHeight.value = calcTreeFullHeight() + 'px'
-  let upHiddenCount = calcCount(0, vtreeWrapDom.scrollTop)
+  const [ upHiddenCount, calcItemHeight, lastItemHeight ] = calcUpHiddenCount(0, vtreeWrapDom.scrollTop)
+  /**
+   * v-tree 每次 translateY() 下移的距离应该是 calcItemHeight - lastItemHeight 
+   * 其中 calcItemHeight 是刚好超出 scrollTop 的总dom高度
+   * lastItemHeight 是包含在scrollTop中的最后一个列表项lastItem的高度，最后这个列表项有可能完全包含在scrollTop中，
+   * 也有可能超出scrollTop
+   * 这样设置 translateY()的值，就能保证当一次滚动结束后，当lastItem被遮住一部分的情况时，整个v-tree内容区域就能刚好
+   * 停在遮住一部分这种状态。而在计算upHiddenCount时，如果采用向上取整，则当遇到这种lastItem遮住一部分的情况时，v-tree内容区
+   * 就会发生跳跃，从"遮住lastItem一部分" 这个状态跳跃到 “从lastItem+1开始完整展示”
+   */
+  vtreeMoveDistance.value = Math.max(calcItemHeight - lastItemHeight, 0)
   start.value = Math.max(upHiddenCount, 0)
 
   let viewportCount = calcCount(start.value, vtwHeight)
-  // const vtreeDom = document.querySelector('.virtual-tree-wrapper .v-tree')
-  // for (let i = start.value; i < start.value + viewportCount; i++) {
-  //   const data = treeMap[i][1]
-  //   const itemDom = createVTreeItemDom(data)
-  //   vtreeDom.appendChild(itemDom)
-  // }
   renderedTreeData.value = treeMap.slice(start.value, start.value + viewportCount).map(t => t[1])
 
-  const scrollHandle = (evt) => {
+  const scrollHandle = (evt: Event) => {
     const target = evt.target as HTMLDivElement
     if (target.scrollTop === scrollTop.value) return
 
-    let contentMoveUp = target.scrollTop > scrollTop.value // true - 内容上移  false - 内容下移
     scrollTop.value = target.scrollTop
-    upHiddenCount = calcCount(0, scrollTop.value)
-    // 下标 start.value~upHiddenCount-2 [start.value, upHiddenCount - 1)的dom节点移除
-    // const vTreeDom = document.querySelector(`.virtual-tree-wrapper .v-tree`) as HTMLDivElement
-    // if (start.value >= upHiddenCount - 1) return
-
-    // for (let i = start.value; i < upHiddenCount - 1; i++) {
-    //   const itemDom = vTreeDom.firstElementChild
-    //   if (itemDom) {
-    //     vTreeDom.removeChild(itemDom)
-    //     treeFullDomList[i] = itemDom
-    //   }
-    // }
-
+    const [ upHiddenCount, calcItemHeight, lastItemHeight ] = calcUpHiddenCount(0, scrollTop.value)
+    vtreeMoveDistance.value = Math.max(calcItemHeight - lastItemHeight, 0)
     // 更新起始下标start.value
-    // start.value = Math.max(upHiddenCount - 1, 0)
     start.value = upHiddenCount
 
     viewportCount = calcCount(start.value, vtwHeight)
     renderedTreeData.value = treeMap.slice(start.value, start.value + viewportCount).map(t => t[1])
-
-    // for (let i = start.value; i < start.value + viewportCount; i++) {
-    //   const data = treeMap[i][1]
-    //   const itemDom = createVTreeItemDom(data)
-    //   vtreeDom.appendChild(itemDom)
-    // }
   }
-  vtreeWrapDom.addEventListener('scroll', scrollHandle)
+
+  let ticking = false
+
+  vtreeWrapDom.addEventListener('scroll', (evt: Event) => {
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        scrollHandle(evt)
+        ticking = false
+      })
+
+      ticking = true
+    }
+  })
 }
 
 onMounted(() => {
@@ -183,6 +173,7 @@ onMounted(() => {
      * 而是用于撑起容器 dom 滚动条的 treeFullHeight 高度，不知道为何。
      * 因此把 calculateVirtualTreeData() 放在下一轮事件循环再执行，让页面先渲染一次，保证设置的样式 height:calc(100% - 104px) 生效后，
      * 再来获取容器 dom 的高度值。
+     * 该问题在vue 3.3.4 版本中不存在
      */
     calculateVirtualTreeData()
   });
